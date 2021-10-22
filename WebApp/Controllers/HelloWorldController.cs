aae1b80db2029c725d403c74b86674bff162da63
szymon-kiciak <szymon.kiciak@gmail.com>
2021-10-22 11:21:41 +0100

    Added extra controller

---
+++ WebApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloWorldController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult SayHello()
+        {
+            return Ok("Hello from SayHello");
+        }
+    }
+}
